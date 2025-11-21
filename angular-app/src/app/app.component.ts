c135682d39e093435efef37507f09398314352ab
matt-fonto <mateus.fonto@gmail.com>
2025-11-21 19:47:51 +0100

    routing

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -6,11 +6,11 @@ import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   template: `
     <app-header />
     <main>
-      <app-home />
+      <router-outlet />
       <app-footer />
     </main>
   `,
--- angular-app/src/app/app.routes.ts
+++ angular-app/src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    // loadComponent provides lazy-loading
+    loadComponent: () =>
+      import('./home/home.component').then((m) => m.HomeComponent),
+    // while
+    // component: HomeComponent doesn't
+  },
+  {
+    path: 'about',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('./about/about.component').then((m) => m.AboutComponent),
+  },
+  {
+    path: 'contact',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('./contact/contact.component').then((m) => m.ContactComponent),
+  },
+];
