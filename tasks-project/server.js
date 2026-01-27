a1a115ca6b106cfced03dbb6096773c98b59d880
matt-fonto <mateus.fonto@gmail.com>
2026-01-27 19:28:15 +0100

    update server

--- tasks-project/server.js
+++ tasks-project/server.js
@@ -1,22 +1,44 @@
+const PORT = 5001;
+const DB_FILE = "db.json";
+const HEADER_CONFIG = {
+  allow_origins: {
+    title: "Access-Control-Allow-Origin",
+    permission: "*",
+  },
+  allow_methods: {
+    title: "Access-Control-Allow-Methods",
+    permission: "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+  },
+  allow_headers: {
+    title: "Access-Control-Allow-Headers",
+    permission: "Origin, X-Requested-With, Content-Type, Accept",
+  },
+};
+
+const ALLOW_ORIGINS = "";
+
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json"); // Your database file
+const router = jsonServer.router(DB_FILE);
 
-// Add custom middleware for CORS
 server.use((_, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // Allow any origin
   res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+    HEADER_CONFIG.allow_origins.title,
+    HEADER_CONFIG.allow_origins.permission,
   );
   res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept",
+    HEADER_CONFIG.allow_methods.title,
+    HEADER_CONFIG.allow_methods.permission,
   );
+  res.header(
+    HEADER_CONFIG.allow_headers.title,
+    HEADER_CONFIG.allow_headers.permission,
+  );
+
   next();
 });
 
 server.use(router);
-server.listen(5001, () => {
-  console.log("JSON Server is running");
+server.listen(PORT, () => {
+  console.log(`Server port: ${PORT}\n---\nServer db: ${DB_FILE}`);
 });
